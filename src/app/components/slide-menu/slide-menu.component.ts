8984c382dca4982ae1e496a763d718feec92eed2
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-02-24 09:38:18 -0500

    (FEATURE): Slide-menu was created

---
+++ src/app/components/slide-menu/slide-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-menu',
+  imports: [],
+  templateUrl: './slide-menu.component.html',
+  styleUrl: './slide-menu.component.css'
+})
+export class SlideMenuComponent {
+
+}
--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { SlideMenuComponent } from "../../components/slide-menu/slide-menu.component";
 
 @Component({
   selector: 'app-products-list',
-  imports: [NavbarComponent, NavbarComponent],
+  imports: [
+    NavbarComponent,
+    NavbarComponent,
+    SlideMenuComponent
+  ],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
