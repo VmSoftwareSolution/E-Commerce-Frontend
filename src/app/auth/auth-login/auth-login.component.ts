3508bafb6cfe0947638b5d528b50a53b87b9667a
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-02-25 12:38:15 -0500

    (REFACTOR): New implementation to set and get token

--- src/app/auth/auth-login/auth-login.component.ts
+++ src/app/auth/auth-login/auth-login.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../service/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogModalComponent } from '../../components/dialog-modal/dialog-modal.component';
 import { Router } from '@angular/router';
+import { TokenService } from '../../service/token.service';
 
 @Component({
   selector: 'app-auth-login',
@@ -22,12 +23,15 @@ export class AuthLoginComponent {
    * @param fb - FormBuilder for creating reactive forms.
    * @param authService - Service to handle authentication operations.
    * @param dialog - Material Dialog service to display modal dialogs.
+   * @param router - Router for navigation.
+   * @param tokenService - Service to handle token storage.
   */
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private dialog: MatDialog,
     private router: Router,
+    private tokenService: TokenService
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -38,7 +42,7 @@ export class AuthLoginComponent {
   /**
    * Handles the submission of the login form.
    * It marks all controls as touched if the form is invalid.
-   * If valid, it calls the AuthService to perform login, sets a cookie with the token,
+   * If valid, it calls the AuthService to perform login, sets the token in local storage,
    * and displays a success dialog. On error, an error dialog is shown.
   */
   onSubmit() {
@@ -50,20 +54,20 @@ export class AuthLoginComponent {
     // Call method Login
     this.authService.login(this.loginForm.value).subscribe({
       next: (token) => {
-        document.cookie = `jwToken=${JSON.stringify(token)};path=/;Max-Age=86400`;
+        this.tokenService.setToken(token.token);
 
         const dialogRef = this.dialog.open(DialogModalComponent,{
           data: {
             tittle: 'Bienvenido',
             message: 'Usuario autenticado correctamente',
           }
-        })
+        });
 
         dialogRef.afterClosed().subscribe(() => {
           this.router.navigate(['listProducts']);
         });
       },
-      error: (error) => {
+      error: () => {
         this.dialog.open(DialogModalComponent, {
           data: {
             message: "Usuario o contraseña incorrectas",
@@ -82,7 +86,6 @@ export class AuthLoginComponent {
   get email() {
     return this.loginForm.get('email');
   }
-
 
   /**
    * Getter for the password form control.
